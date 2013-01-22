a9373c20db9d056ad031095cee85036db1d81c53
Irfan Durmus <irfandurmus@gmail.com>
2013-01-23 02:57:27 +0400

    toString() added before replace to ensure its type

--- src/core.js
+++ src/core.js
@@ -516,7 +516,8 @@ jQuery.extend({
 	// Convert dashed to camelCase; used by the css and data modules
 	// Microsoft forgot to hump their vendor prefix (#9572)
 	camelCase: function( string ) {
-		return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
+        // if you passing number instead string it throws error
+		return string.toString().replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
 	},
 
 	nodeName: function( elem, name ) {
